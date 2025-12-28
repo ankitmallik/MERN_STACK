293c1d61bbc03aeab36a1bcb241dfb14018c39bb
Ankit Kumar <ankitmallik33@gmail.com>
2025-12-29 01:29:20 +0530

    promise

--- promise.js
+++ promise.js
@@ -1,11 +1,22 @@
 //  Creating a Promise that resolves after 2 seconds with a username object
-const firstPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve({username: "ankit mallik"})
-  }, 2000);
-});
-firstPromise.then((username)=>
-{
-    console.log(username);
-    
-})
+// const firstPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve({username: "ankit mallik"})
+//   }, 2000);
+// });
+// firstPromise.then((username)=>
+// {
+//     console.log(username);
+
+// })
+
+fetch("https://dummyjson.com/users")
+  .then((res) => {
+    return res.json();
+  })
+  .then((res) => {
+    console.log(res.users[0].address.coordinates);
+  })
+  .catch(() => {
+    console.log("error");
+  });
